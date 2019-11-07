d2e96bc6ec033bc13114edd232bc437baac08caa
Richárd Németh <nemethricsi@gmail.com>
2019-11-07 12:50:42 +0100

    add Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,13 @@
-import React, { Fragment } from "react";
-import { fade, makeStyles } from "@material-ui/core/styles";
-import { AppBar, Button, Avatar } from "@material-ui/core";
+import React from "react";
+import PropTypes from 'prop-types';
+import { makeStyles } from "@material-ui/core/styles";
+import { AppBar, Button, IconButton, MenuItem, Menu, Link } from "@material-ui/core";
 import Toolbar from "@material-ui/core/Toolbar";
-import IconButton from "@material-ui/core/IconButton";
-import Typography from "@material-ui/core/Typography";
-import InputBase from "@material-ui/core/InputBase";
-import Badge from "@material-ui/core/Badge";
-import MenuItem from "@material-ui/core/MenuItem";
-import Menu from "@material-ui/core/Menu";
-import MenuIcon from "@material-ui/icons/Menu";
-import SearchIcon from "@material-ui/icons/Search";
 import AccountCircleRoundedIcon from '@material-ui/icons/AccountCircleRounded';
-import MailIcon from "@material-ui/icons/Mail";
-import NotificationsIcon from "@material-ui/icons/Notifications";
-import MoreIcon from "@material-ui/icons/MoreVert";
 import ExitToAppRoundedIcon from "@material-ui/icons/ExitToAppRounded";
-import Logo_white from '../images/logo_white.png';
-import Logo from '../images/logo.png';
-import Akos from '../images/avatars/Akos.png';
 import LockOpenRoundedIcon from '@material-ui/icons/LockOpenRounded';
 import EmojiPeopleRoundedIcon from '@material-ui/icons/EmojiPeopleRounded';
+import Logo_white from '../images/logo_white.png';
 
 const useStyles = makeStyles(theme => ({
   grow: {
@@ -28,47 +16,6 @@ const useStyles = makeStyles(theme => ({
   button: {
     marginRight: theme.spacing(2),
     padding: theme.spacing(1, 4)
-  },
-  title: {
-    display: "none",
-    [theme.breakpoints.up("sm")]: {
-      display: "block"
-    }
-  },
-  search: {
-    position: "relative",
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    "&:hover": {
-      backgroundColor: fade(theme.palette.common.white, 0.25)
-    },
-    marginRight: theme.spacing(2),
-    marginLeft: 0,
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      marginLeft: theme.spacing(3),
-      width: "auto"
-    }
-  },
-  searchIcon: {
-    width: theme.spacing(7),
-    height: "100%",
-    position: "absolute",
-    pointerEvents: "none",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  inputRoot: {
-    color: "inherit"
-  },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 7),
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("md")]: {
-      width: 200
-    }
   },
   sectionDesktop: {
     display: "none",
@@ -87,32 +34,23 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-export default function Navbar() {
+export default function Navbar(props) {
   const classes = useStyles();
-  const [anchorEl, setAnchorEl] = React.useState(null);
   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
-
-  const isMenuOpen = Boolean(anchorEl);
   const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
-
-  const isLoggedIn = true;
-
-  const handleProfileMenuOpen = event => {
-    setAnchorEl(event.currentTarget);
-  };
+  const { isLoggedIn } = props;
 
   const handleMobileMenuClose = () => {
     setMobileMoreAnchorEl(null);
   };
 
-  const handleMenuClose = () => {
-    setAnchorEl(null);
-    handleMobileMenuClose();
-  };
-
   const handleMobileMenuOpen = event => {
     setMobileMoreAnchorEl(event.currentTarget);
   };
+
+  const handleLogout = () => {
+    console.log('kijelentkeztem');
+  }
 
   const mobileMenuId = "primary-search-account-menu-mobile";
   const renderMobileMenu = (
@@ -126,35 +64,41 @@ export default function Navbar() {
       onClose={handleMobileMenuClose}
     >
       {isLoggedIn ? (
-        <Fragment>
-          <MenuItem onClick={handleProfileMenuOpen}>
-            <IconButton aria-label="show 4 new mails" color="inherit">
-              <AccountCircleRoundedIcon />
-            </IconButton>
-            <p>Fiókom</p>
-          </MenuItem>
-          <MenuItem>
+        <div>
+          <Link underline="none" color="inherit" href="/account">
+            <MenuItem>
+              <IconButton aria-label="show 4 new mails" color="inherit">
+                <AccountCircleRoundedIcon />
+              </IconButton>
+              <p>Fiókom</p>
+            </MenuItem>
+          </Link>
+          <MenuItem onClick={handleLogout}>
             <IconButton aria-label="show 4 new mails" color="inherit">
               <ExitToAppRoundedIcon />
             </IconButton>
             <p>Kijelentkezés</p>
           </MenuItem>
-        </Fragment>
+        </div>
       ) : (
-          <Fragment>
-            <MenuItem onClick={handleProfileMenuOpen}>
-              <IconButton aria-label="show 4 new mails" color="inherit">
-                <LockOpenRoundedIcon />
-              </IconButton>
-              <p>Bejelentkezés</p>
-            </MenuItem>
-            <MenuItem>
-              <IconButton aria-label="show 4 new mails" color="inherit">
-                <EmojiPeopleRoundedIcon />
-              </IconButton>
-              <p>Regisztráció</p>
-            </MenuItem>
-          </Fragment>
+          <div>
+            <Link underline="none" color="inherit" href="/login">
+              <MenuItem>
+                <IconButton aria-label="show 4 new mails" color="inherit">
+                  <LockOpenRoundedIcon />
+                </IconButton>
+                <p>Bejelentkezés</p>
+              </MenuItem>
+            </Link>
+            <Link underline="none" color="inherit" href="/register">
+              <MenuItem>
+                <IconButton aria-label="show 4 new mails" color="inherit">
+                  <EmojiPeopleRoundedIcon />
+                </IconButton>
+                <p>Regisztráció</p>
+              </MenuItem>
+            </Link>
+          </div>
         )}
     </Menu>
   );
@@ -168,16 +112,31 @@ export default function Navbar() {
           </a>
           <div className={classes.grow} />
           <div className={classes.sectionDesktop}>
-            <Button className={classes.button} color="inherit">
-              Login
-            </Button>
-            <Button
-              className={classes.button}
-              color="secondary"
-              variant="contained"
-            >
-              Join
-            </Button>
+            {isLoggedIn ? (
+              <div>
+                <Button href="/account" startIcon={<AccountCircleRoundedIcon />} className={classes.button} color="inherit">
+                  Fiókom
+                </Button>
+                <Button onClick={handleLogout} startIcon={<ExitToAppRoundedIcon />} className={classes.button} color="inherit"
+                >
+                  Kijelentkezés
+                </Button>
+              </div>
+            ) : (
+                <div>
+                  <Button href="/login" className={classes.button} color="inherit">
+                    Bejelentkezés
+          </Button>
+                  <Button
+                    href="/register"
+                    className={classes.button}
+                    color="secondary"
+                    variant="contained"
+                  >
+                    Regisztráció
+          </Button>
+                </div>
+              )}
           </div>
           <div className={classes.sectionMobile}>
             <IconButton
@@ -187,6 +146,7 @@ export default function Navbar() {
               onClick={handleMobileMenuOpen}
               color="inherit"
             >
+              {/* TODO: user Avatar photo here? */}
               <AccountCircleRoundedIcon />
             </IconButton>
           </div>
@@ -196,3 +156,7 @@ export default function Navbar() {
     </div>
   );
 }
+
+Navbar.propTypes = {
+  isLoggedIn: PropTypes.bool,
+};
