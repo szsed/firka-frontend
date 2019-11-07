740b18261f9f80ac5b9a68537f089244f44ebaff
Németh Dávid <ndndnd24@gmail.com>
2019-11-07 10:57:55 +0100

    file content initialization

---
+++ src/store/games.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const initialState = {
+
+};
+
+const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case '':
+      return {
+
+      }
+  };
+  return state;
+}
+
+export default userReducer;
