6f771e3c1870cc2936367f05875ea4db5dee3080
Ákos Fekete <feketeakos23@users.noreply.github.com>
2019-11-07 14:06:55 +0100

    some improvements

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { CssBaseline } from "@material-ui/core";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import { makeStyles } from "@material-ui/core/styles";
@@ -7,38 +8,36 @@ import TrendingUpRoundedIcon from "@material-ui/icons/TrendingUpRounded";
 import ContactSupportRoundedIcon from "@material-ui/icons/ContactSupportRounded";
 
 const useStyles = makeStyles({
-  root: {
-    flexGrow: 2,
-    display: "flex",
-    justifyContent: "space-around",
-    position: "absolute",
-    rigth: 0,
-    bottom: 0,
-    left: 0,
-  }
+    root: {
+        flexGrow: 2,
+        display: "flex",
+        justifyContent: "space-around",
+    }
 });
 
 export default function Footer() {
-  const classes = useStyles();
-  const [value, setValue] = React.useState(0);
+    const classes = useStyles();
+    const [value, setValue] = React.useState(0);
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
 
-  return (
-    <Tabs
-      className={classes.root}
-      value={value}
-      onChange={handleChange}
-      variant="fullWidth"
-      indicatorColor="secondary"
-      textColor="secondary"
-      aria-label="icon label tabs example"
-    >
-      <Tab icon={<DashboardRoundedIcon />} label="Főoldal" />
-      <Tab icon={<TrendingUpRoundedIcon />} label="Ranglista" />
-      <Tab icon={<ContactSupportRoundedIcon />} label="Szabályok" />
-    </Tabs>
+    return (
+        <CssBaseline>
+        <Tabs
+            className={classes.root}
+            value={value}
+            onChange={handleChange}
+            variant="fullWidth"
+            indicatorColor="secondary"
+            textColor="secondary"
+            aria-label="icon label tabs example"
+        >
+            <Tab icon={<DashboardRoundedIcon />} label="Főoldal" />
+            <Tab icon={<TrendingUpRoundedIcon />} label="Ranglista" />
+            <Tab icon={<ContactSupportRoundedIcon />} label="Szabályok" />
+        </Tabs>
+        </CssBaseline>
   );
 }
