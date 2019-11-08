aba26b02c3b7878d13a2f4e090742883c0f56fbf
Richárd Németh <nemethricsi@gmail.com>
2019-11-08 03:11:00 +0100

    add Account page

--- src/pages/account.js
+++ src/pages/account.js
@@ -1,15 +1,88 @@
 import React, { Component, Fragment } from 'react';
-import { CssBaseline } from '@material-ui/core';
+import { CssBaseline, Container, Paper, Typography, Button, Avatar } from '@material-ui/core';
+import { withStyles } from "@material-ui/core/styles";
+import { connect } from 'react-redux';
 
-class AccountPage extends Component {
+const useStyles = theme => ({
+  games: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+    '& > *': {
+      margin: theme.spacing(0.5),
+    },
+    marginBottom: theme.spacing(6),
+  },
+  paper: {
+    marginTop: theme.spacing(3),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    textAlign: "center",
+    padding: theme.spacing(4),
+    position: 'relative',
+  },
+  title: {
+    fontSize: 24,
+    margin: theme.spacing(1),
+    [theme.breakpoints.up("md")]: {
+      fontSize: 28,
+      marginBottom: theme.spacing(4),
+    }
+  },
+  button: {
+    margin: theme.spacing(3, 0, 2),
+    padding: theme.spacing(2),
+  },
+  boxContainer: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    margin: theme.spacing(4),
+    width: '100%',
+    [theme.breakpoints.up("sm")]: {
+      width: '90%',
+      justifyContent: 'space-around',
+      margin: theme.spacing(8, 2),
+    }
+  },
+  icon: {
+    fontSize: 50,
+  },
+  avatar: {
+    width: 150,
+    height: 150,
+  }
+});
+
+class Account extends Component {
+  constructor(props) {
+    super(props);
+  };
+
   render() {
+    const { classes, user } = this.props;
+    if (!user) return null;
     return (
       <Fragment>
         <CssBaseline />
-        <h1>Fiókom</h1>
+        <Container maxWidth="sm">
+          <Paper className={classes.paper}>
+            <Avatar
+              className={classes.avatar}
+              src={user.playerDetails.avatar}
+            />
+            <Typography className={classes.title}>{user.playerDetails.username}</Typography>
+            <Button className={classes.button} variant="contained" >Adatok szerkesztése</Button>
+            <Button href="/" className={classes.button} >Vissza a főoldalra</Button>
+          </Paper>
+        </Container>
       </Fragment>
     );
   }
 }
 
-export default AccountPage;
+const mapStateToProps = state => ({
+  user: state.user,
+});
+
+export default connect(mapStateToProps, null)(withStyles(useStyles)(Account));
