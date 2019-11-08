d71f1ad4707eca85cdfc9cf18d3831ca2e822989
Ákos Fekete <feketeakos23@users.noreply.github.com>
2019-11-08 01:57:51 +0100

    feat(leaderboard) created

--- src/pages/leaderboard.js
+++ src/pages/leaderboard.js
@@ -1,15 +1,162 @@
 import React, { Component, Fragment } from 'react';
-import { CssBaseline } from '@material-ui/core';
+import Navbar from '../components/Navbar';
+import { CssBaseline, Container, Paper, Typography, Button, Avatar, TextField, Chip } from '@material-ui/core';
+import { withStyles, makeStyles, createMuiTheme } from "@material-ui/core/styles";
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
+import { requestToAPI } from "../services/backend-api-services";
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    marginTop: theme.spacing(3),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    textAlign: "center",
+    position: 'relative',
+    padding: theme.spacing(4),
+  },
+  root: {
+    width: '100%',
+  },
+  tableWrapper: {
+    maxHeight: 440,
+    overflow: 'auto',
+  },
+}));
+const rows = [
+  // 'India', 'IN', 1324171354,
+];
+console.log(rows)
+
+const columns = [
+  { id: 'avatar', label: 'Profil', minWidth: 170, align: 'center', },
+  { id: 'username', label: 'Felhasználó', minWidth: 100 },
+  { id: 'victories', label: 'Pontszám', minWidth: 170, align: 'center' },
+];
+
+const fillRows = (parsed) => {
+  return parsed.map(userData => ({
+    avatar: userData.avatar,
+    username: userData.username,
+    victories: userData.victories,
+  }))
+}
+
+const getUserData = () => {
+  return requestToAPI('/users', {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+  })
+    .then(response => response.json())
+    .then(parsed => {
+      if (parsed.message) throw parsed;
+      return fillRows(parsed)
+    })
+    .catch(err => {
+      console.log(err.message);
+      return err;
+    });
+}
+
+const classes = useStyles();
+const [page, setPage] = React.useState(0);
+const [rowsPerPage, setRowsPerPage] = React.useState(10);
 
 class Leaderboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rows: '',
+    };
+  };
+
+  handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+  componentDidMount() {
+    getUserData()
+      .then(rows => {
+        this.setState({
+          rows
+        })
+      })
+  }
+
   render() {
     return (
       <Fragment>
         <CssBaseline />
-        <h1>Ranglista</h1>
+        <Navbar />
+        <Container maxWidth="sm">
+          <Paper className={classes.paper}>
+            <div className={classes.tableWrapper}>
+              <Table stickyHeader aria-label="sticky table">
+                <TableHead>
+                  <TableRow>
+                    {columns.map(column => (
+                      <TableCell
+                        key={column.id}
+                        align={column.align}
+                        style={{ minWidth: column.minWidth }}
+                      >
+                        {column.label}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {this.state.rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(row => {
+                    return (
+                      <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                        {columns.map(column => {
+                          const value = row[column.id];
+                          return (
+                            <TableCell key={column.id} align={column.align}>
+                              {column.format && typeof value === 'number' ? column.format(value) : value}
+                            </TableCell>
+                          );
+                        })}
+                      </TableRow>
+                    );
+                  })}
+                </TableBody>
+              </Table>
+            </div>
+            <TablePagination
+              rowsPerPageOptions={[10, 25, 100]}
+              component="div"
+              count={rows.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              backIconButtonProps={{
+                'aria-label': 'Előző oldal',
+              }}
+              nextIconButtonProps={{
+                'aria-label': 'Következő oldal',
+              }}
+              onChangePage={handleChangePage}
+              onChangeRowsPerPage={handleChangeRowsPerPage}
+            />
+          </Paper>
+        </Container>
       </Fragment>
     );
   }
 }
+
+
 
 export default Leaderboard;
