49288b77be15d2a5e4495f82110cecd3d418683d
Pascal Dawson <dawsonpascal@hotmail.com>
2019-11-22 00:25:06 +0100

    should be working mostly

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -26,32 +26,6 @@ class Canvas extends Component {
     let top = canvasPos.top;
     let left = canvasPos.left;
 
-
-    /* function colorForTouch(touch) {
-      var r = touch.identifier % 16;
-      var g = Math.floor(touch.identifier / 3) % 16;
-      var b = Math.floor(touch.identifier / 7) % 16;
-      r = r.toString(16); // make it a hex digit
-      g = g.toString(16); // make it a hex digit
-      b = b.toString(16); // make it a hex digit
-      let color = "#" + r + g + b;
-      return color;
-    }
-
-    function copyTouch({ identifier, pageX, pageY }) {
-      return { identifier, pageX, pageY };
-    }
-
-    function ongoingTouchIndexById(idToFind) {
-      for (let i = 0; i < ongoingTouches.length; i++) {
-        let id = ongoingTouches[i].identifier;
-        if (id == idToFind) {
-          return i;
-        }
-      }
-      return -1;    // not found
-    } */
-
     canvas.addEventListener('mousedown', (event) => {
       let mouseX = event.offsetX;
       let mouseY = event.offsetY;
@@ -60,30 +34,6 @@ class Canvas extends Component {
       addClick(mouseX, mouseY);
       redraw();
     }, false);
-
-    function handleStart(event) {
-      event.preventDefault();
-      let touches = event.changedTouches;
-
-      let touchX = touches[touches.length - 1].pageX - top;
-      let touchY = touches[touches.length - 1].pageX - left;
-      if (paint) {
-        addClick(touchX, touchY, true);
-        redraw();
-      }
-      /*  let touches = event.changedTouches;
- 
-       for (let i = 0; i < touches.length; i++) {
-         ongoingTouches.push(copyTouch(touches[i]));
-         let color = colorForTouch(touches[i]);
-         context.beginPath();
-         context.arc(touches[i].pageX, touches[i].pageY, 4, 0, 2 * Math.PI, false);  // a circle at the start
-         context.fillStyle = color;
-         context.fill();
-       } */
-    }
-
-    canvas.addEventListener('touchstart', handleStart, false)
 
     canvas.addEventListener('mousemove', (event) => {
       let mouseX = event.offsetX;
@@ -94,82 +44,40 @@ class Canvas extends Component {
       }
     }, false)
 
-    function handleMove(event) {
-      event.preventDefault();
-      let touches = event.changedTouches;
-
-      let touchX = touches[touches.length - 1].pageX - top;
-      let touchY = touches[touches.length - 1].pageX - left;
-      if (paint) {
-        addClick(touchX, touchY, true);
-        redraw();
-      }
-
-      /*  for (let i = 0; i < touches.length; i++) {
-        let color = colorForTouch(touches[i]);
-        let idx = ongoingTouchIndexById(touches[i].identifier);
-
-        if (idx >= 0) {
-          context.beginPath();
-          context.moveTo(ongoingTouches[idx].clientX, ongoingTouches[idx].clientY);
-          context.lineTo(touches[i].clientX + canvasOffsetTop, touches[i].clientY + canvasOffsetLeft);
-          context.lineWidth = 4;
-          context.strokeStyle = color;
-          context.stroke();
-
-          ongoingTouches.splice(idx, 1, copyTouch(touches[i]));  // swap in the new touch record
-        } else {
-          console.log("can't figure out which touch to continue");
-        }
-      } */
-    }
-
-    canvas.addEventListener('touchmove', handleMove, false)
-
     canvas.addEventListener('mouseup', (e) => {
       paint = false;
     }, false);
-
-
-    /* function handleEnd(event) {
-      event.preventDefault();
-      let touches = event.changedTouches;
- 
-      for (let i = 0; i < touches.length; i++) {
-        let color = colorForTouch(touches[i]);
-        let idx = ongoingTouchIndexById(touches[i].identifier);
- 
-        if (idx >= 0) {
-          context.lineWidth = 4;
-          context.fillStyle = color;
-          context.beginPath();
-          context.moveTo(ongoingTouches[idx].pageX, ongoingTouches[idx].pageY);
-          context.lineTo(touches[i].pageX, touches[i].pageY);
-          context.fillRect(touches[i].pageX - 4, touches[i].pageY - 4, 8, 8);  // and a square at the end
-          ongoingTouches.splice(idx, 1);  // remove it; we're done
-        } else {
-          console.log("can't figure out which touch to end");
-        }
-      }
-    }
- 
-    canvas.addEventListener('touchend', handleEnd, false); */
 
     canvas.addEventListener('mouseleave', (e) => {
       paint = false;
     }, false);
 
-    /* function handleCancel(event) {
+    canvas.addEventListener('touchstart', (event) => {
+      let touchX = (event.changedTouches ? event.changedTouches[0].pageX : event.pageX) - left;
+      let touchY = (event.changedTouches ? event.changedTouches[0].pageY : event.pageY) - top;
+      paint = true
+      addClick(touchX, touchY, false);
+      redraw();
+    }, false)
+
+    canvas.addEventListener('touchmove', (event) => {
       event.preventDefault();
-      let touches = event.changedTouches;
-
-      for (let i = 0; i < touches.length; i++) {
-        let idx = ongoingTouchIndexById(touches[i].identifier);
-        ongoingTouches.splice(idx, 1);  // remove it; we're done
+      let touchX = (event.changedTouches ? event.changedTouches[0].pageX : event.pageX) - left;
+      let touchY = (event.changedTouches ? event.changedTouches[0].pageY : event.pageY) - top;
+      if (paint) {
+        addClick(touchX, touchY, true);
+        redraw();
       }
-    }
+    }, false)
 
-    canvas.addEventListener('touchcancel', handleCancel, false); */
+    canvas.addEventListener("touchend", function (e) {
+      paint = false;
+      redraw();
+    }, false);
+
+    canvas.addEventListener("touchcancel", function (e) {
+      paint = false;
+    }, false);
 
     const strokeColor = "black";
     const strokeWidt = 5;
@@ -232,18 +140,18 @@ class Canvas extends Component {
       <>
         <canvas className="canvas" id="canvas" width={this.props.width} height={this.props.height}></canvas>
         <div className="colorButtons">
-          {/*  <button id="red" className='colorButton'>Piros</button>
+          <button id="red" className='colorButton'>Piros</button>
           <button id="yellow" className='colorButton'>Sárga</button>
           <button id="orange" className='colorButton'>Narancs</button>
           <button id="blue" className='colorButton'>Kék</button>
           <button id="green" className='colorButton'>Zöld</button>
           <button id="brown" className='colorButton'>Barna</button>
-          <button id="black" className='colorButton'>Fekete</button> */}
+          <button id="black" className='colorButton'>Fekete</button>
         </div>
         <div className="sizeButtons">
-          {/* <button id="2" className='sizeButton'>Kicsi</button>
+          <button id="2" className='sizeButton'>Kicsi</button>
           <button id="5" className='sizeButton'>Közepes</button>
-          <button id="10" className='sizeButton'>Nagy</button> */}
+          <button id="10" className='sizeButton'>Nagy</button>
         </div>
       </>
     )
